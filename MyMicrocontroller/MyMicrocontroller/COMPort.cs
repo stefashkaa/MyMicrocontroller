9f2f7e1b3be787fa2a977fb72c0e28f4de3265ea
Stefan <stefanpopov@list.ru>
2017-09-29 01:07:15 +0400

    Add port initialization

---
+++ MyMicrocontroller/MyMicrocontroller/COMPort.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.Ports;
+
+namespace MyMicrocontroller
+{
+    public class COMPort
+    {
+        private SerialPort serial;
+
+        public SerialPort Serial { get => serial; set => serial = value; }
+
+        public COMPort(string name)
+        {
+            serial = new SerialPort(name, 38400, Parity.None, 8, StopBits.One);
+        }
+        /*
+         * serial.Open();
+         * Поле Serial.isOpen позволяет проверить нам, открыт ли COM порт.
+         * Работать с COM портом мы можем как по таймеру, с определенным интервалом, так и по событию DataReceived.
+         * serial.Read(byte[] Buffer,int offset,int count) — считать count байт в массив Buffer, со смещением offset.
+         * serial.ReadByte() — считать один байт.
+         * serial.ReadChar() — считать один символ.
+         * serial.ReadExisting() — считать строкой все содержимое буфера.
+         * serial.ReadLine() — считать строку из буфера до символа Serial.NewLine .
+         * serial.ReadTo(String str) — считать содержимое буфера, до достижения строки str.
+         * serial.Write(byte[] Buffer,int offset,int count) — записать count байт массива Buffer, со смещением offset.
+         * serial.WriteLine(string text) — записать строку text в порт.
+         * serial.Close() - по окончанию.
+         */
+    }
+}
--- MyMicrocontroller/MyMicrocontroller/Client.xaml.cs
+++ MyMicrocontroller/MyMicrocontroller/Client.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +24,30 @@ namespace MyMicrocontroller
         public Client()
         {
             InitializeComponent();
+            InitializeCOMSelector();
         }
 
         public Client(string name) : this()
         {
             this.userName = name;
+        }
+
+        private void InitializeCOMSelector()
+        {
+            var ports = SerialPort.GetPortNames();
+            portsSelector.Items.Clear();
+            foreach (var port in ports)
+            {
+                portsSelector.Items.Add(port);
+            }
+            portsSelector.SelectedIndex = 0;
+        }
+
+        private COMPort InitializeSerial()
+        {
+            var name = (string)portsSelector.SelectedItem;
+            var serial = new COMPort(name);
+            return serial;
         }
     }
 }
